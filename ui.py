5576448cf8b1eab84d0dda68a83129b3f5e805ea
mmilan <milanmathew205@gmail.com>
2025-06-26 17:39:13 +0800

    created open path button and ability to browse directories

--- ui.py
+++ ui.py
@@ -3,9 +3,26 @@
 #
 
 import tkinter
+from tkinter import filedialog
 
 window = tkinter.Tk()
 window.title("File Management")
 window.geometry("400x350")
+
+displayPath = tkinter.Text(window, height=10)
+#displayPath.grid(column=0, row=0, sticky='nsew')
+
+
+#openDirPath() -> open file browsing dialog when open path button is clicked
+
+def openDirPath():
+    selectedDir = filedialog.askdirectory()
+
+# button layout created
+
+mainButton = tkinter.Button(window, text='Open Path', width=30, justify='left', command=openDirPath)
+mainButton.pack()
+mainButton.place(x=90, y=70)
+
 
 window.mainloop()
